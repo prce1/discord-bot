e12b93834c11862d0a9927d9d0318799d772cb7b
prce1 <ivanprtljaga@gmail.com>
2018-11-09 15:02:09 +0100

    chore: overall functionality

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 /* eslint no-unused-vars: ["error", { "argsIgnorePattern": "^_" }] */
 
 require('dotenv').config();
+
 const fetch = require('node-fetch');
 const Discord = require('discord.js');
 const isString = require('lodash/isString');
 const ytdl = require('ytdl-core');
 
-
 const botID = process.env.BOT_ID;
 const botName = process.env.BOT_NAME;
+const discordToken = process.env.CLIENT_TOKEN;
 const groupName = process.env.DISCORD_GROUP_NAME;
-const googleKey = process.env.GOOGLE_TOKEN;
-const darkSkyKey = process.env.DARKSKY_TOKEN;
+
 const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`${groupName} bot is up and running`); // eslint-disable-line no-console
-  client.channels.find('name', 'general')
-  .send(`${groupName} bot is up and running.`);
-  client.channels.find('name', 'general')
-  .send(`If you want a list of all commands, type "@${botName} help".`);
+  // client.channels.find('name', 'general')
+  // .send(`${groupName} bot is up and running.`);
+  // client.channels.find('name', 'general')
+  // .send(`If you want a list of all commands, type "@${botName} help".`);
 });
 
 class StraightResponseError extends Error {}
@@ -38,7 +38,7 @@ const HANDLERS = [[
   'ping',
   message => message.channel.send('Got ping.'),
 ], [
-  /^play ([^]+)$/i,
+  /^play (.+)$/i,
   async (message, [_, link]) => {
     if (message.member.voiceChannel) {
       const streamOptions = { seek: 0, volume: 1 };
@@ -54,34 +54,44 @@ const HANDLERS = [[
         });
       }).catch(error => console.error(error)); // eslint-disable-line no-console
     } else {
-      message.reply('you need to join a channel first!');
+      message.reply('You need to join a channel first!');
     }
+    message.delete(3000)
+      // eslint-disable-next-line no-console
+      .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+      // eslint-disable-next-line no-console
+      .catch(console.error);
   },
 ], [
   'stop',
   (message) => {
     message.member.voiceChannel.leave();
     client.user.setActivity();
+    message.delete(3000)
+      // eslint-disable-next-line no-console
+      .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+      // eslint-disable-next-line no-console
+      .catch(console.error);
   },
 ], [
   /^get me weather for ([^,]+)(?:, ([^\s]+))?/i,
-  (message, [_, cityL, countryL = 'RS']) => {
+  (message, [_, cityL, country = 'RS']) => {
     const city = cityL.split(' ').join('+');
     fetch(
       `https://maps.googleapis.com/maps/api/geocode/json?address=${city},+${
-        countryL}&key=${googleKey}`
+        country}&key=${process.env.GOOGLE_TOKEN}`
     )
     .then(res => res.json()
-    .then((result) => {
-      if (result.status !== 'OK') {
-        message.channel.send('You have entered an invalid address.');
+    .then((resJson) => {
+      if (resJson.status !== 'OK') {
+        throw new StraightResponseError('You have entered an invalid address.');
       } else {
-        const { lat, lng } = result.results[0].geometry.location;
-        const address = result.results[0].formatted_address;
-        fetch(`https://api.darksky.net/forecast/${darkSkyKey}/${
+        const { lat, lng } = resJson.results[0].geometry.location;
+        const address = resJson.results[0].formatted_address;
+        fetch(`https://api.darksky.net/forecast/${process.env.DARKSKY_TOKEN}/${
           lat
         }, ${lng}?units=si`)
-        .then(resOne => resOne.json())
+        .then(result => result.json())
         .then((response) => {
           const current = response.currently;
           message.channel.send(
@@ -100,7 +110,14 @@ const HANDLERS = [[
             }**`
           );
         })
-        .catch(error => console.error(error)); // eslint-disable-line no-console
+        .catch((error) => {
+          console.error(error); // eslint-disable-line no-console
+          if (error instanceof StraightResponseError) {
+            message.channel.send(error);
+          } else {
+            message.channel.send(`Something went wrong: ${error}`);
+          }
+        });
       }
     }));
   },
@@ -113,7 +130,11 @@ const HANDLERS = [[
       slug,
       dayInput = DAY_OF_WEEK[new Date().getDay()],
       timeInput = new Date().getMinutes() > 45
-        ? (new Date().getHours() + 1)
+        ? (
+            (new Date().getHours() + 1) === 24
+            ? 0
+            : (new Date().getHours() + 1)
+          )
         : new Date().getHours(),
     ]
   ) => {
@@ -130,8 +151,10 @@ const HANDLERS = [[
       if (place) {
         const name = place.meta.name.charAt(0).toUpperCase() +
           place.meta.name.substr(1);
+        const time = parseInt(timeInput, 10) === 24
+          ? 0
+          : parseInt(timeInput, 10);
         let day = DAY_OF_WEEK.indexOf(dayInput);
-        const time = parseInt(timeInput, 10);
         if (day === -1) {
           day = DAY_OF_WEEK.indexOf(`${dayInput}s`);
           if (day === -1) {
@@ -146,13 +169,15 @@ const HANDLERS = [[
         message.channel.send(`Popularity of ${name} on ${dayInput} at ${
           time}h is ${value}/100.\n${value < 20
           ? 'The place *isn\'t popular* at all at this time.'
-          : value < 65
-          ? 'The place *looks promising*, but not really full at this time.'
-          : 'The place is *very hot* at this time!'}`
+          : value < 70
+          ? 'The place has people, but it\'s not really full at this time.'
+          : 'The place is *packed* at this time!'}`
         );
       } else {
         throw new StraightResponseError(
-          `Cannot find place "${slug}".`
+          `Cannot find place "${
+            slug.charAt(0).toUpperCase() + slug.substr(1)
+          }".`
         );
       }
     })
@@ -164,6 +189,32 @@ const HANDLERS = [[
         message.channel.send(`Oops, something went wrong: ${error.message}.`);
       }
     });
+  },
+], [
+  /remind me to (.+) in (\d+) ([^\s]+)/i,
+  (message, [_, reminder, number, identifier = 'minutes']) => {
+    let time = {};
+    identifier.toLowerCase();
+    switch (identifier) {
+      case 'seconds':
+      time = parseInt(number, 10) * 1000;
+      break;
+      case 'minutes':
+      time = parseInt(number, 10) * 60 * 1000;
+      break;
+      case 'hours':
+      time = parseInt(number, 10) * 60 * 60 * 1000;
+      break;
+      case 'days':
+      time = parseInt(number, 10) * 24 * 60 * 60 * 1000;
+      break;
+      default:
+      message.channel.send(`Invalid time: ${number}`);
+    }
+    client.setTimeout(() => {
+      console.log(`Sent reminder: ${reminder}.`); // eslint-disable-line no-console
+      message.author.send(`You need to ${reminder}!`);
+    }, time, message);
   },
 ], [
   'help',
@@ -237,10 +288,15 @@ client.on('guildMemberAdd', (member) => {
   channel.send(`Welcome to ${groupName}, ${member}`);
 });
 
-
 client.on('disconnected', (error) => {
-  console.log(error); // eslint-disable-line no-console
-  client.destroy().then(client.login.bind(client));
+  console.error(error); // eslint-disable-line no-console
+    client.destroy().then(client.login(discordToken));
 });
 
-client.login(process.env.DISCORD_TOKEN);
+client.on('error', (error) => {
+  console.error(error); // eslint-disable-line no-console
+  console.log('reconnecting after an error'); // eslint-disable-line no-console
+    client.destroy().then(client.login(discordToken));
+});
+
+client.login(discordToken);
